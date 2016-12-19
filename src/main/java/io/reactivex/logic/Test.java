454c52594229fd487e843c83ee50067234c0eccb
Matthias van Dijk <matthiasvd2@gmail.com>
2016-12-19 20:20:39 +0100

    Initial Project

---
+++ src/main/java/io/reactivex/logic/Test.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package io.reactivex.logic;
+
+import rx.Observable;
+
+/**
+ *
+ * @author Matthias
+ */
+public class Test {
+    Observable<String> test = Observable.just("Howdy!");
+
+    
+}
