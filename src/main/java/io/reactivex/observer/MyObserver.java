c72298164b3a3699ff6aaccb59559d27b471e20a
Matthias van Dijk <matthiasvd2@gmail.com>
2016-12-19 21:21:18 +0100

    Created a new observer

---
+++ src/main/java/io/reactivex/observer/MyObserver.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package io.reactivex.observer;
+
+import rx.Observable;
+import rx.Observer;
+import rx.Subscriber;
+
+/**
+ *
+ * @author Matthias
+ */
+public class MyObserver implements Observer<String> {
+
+    @Override
+    public void onCompleted() {
+        System.out.println("MyObserver: is klaar en heeft geen fouten ondervonden.");
+    }
+
+    @Override
+    public void onError(Throwable thrwbl) {
+        System.out.println("MyObserver: Error");
+    }
+
+    @Override
+    public void onNext(String t) {
+        System.out.println("MyObserver: " + t);
+    }
+}
