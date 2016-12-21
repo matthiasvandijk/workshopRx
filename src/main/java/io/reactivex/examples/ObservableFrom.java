b5fb45192552a4ee0ee6b2e0b98e1270b8da6405
Matthias van Dijk <matthiasvd2@gmail.com>
2016-12-21 19:10:40 +0100

    update

---
+++ src/main/java/io/reactivex/examples/ObservableFrom.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package io.reactivex.examples;
+
+import java.util.Arrays;
+import java.util.List;
+import io.reactivex.reactive.MyObserver;
+import rx.Observable;
+
+/**
+ *
+ * @author Matthias
+ */
+public class ObservableFrom {
+    public static void main(String[] args) {
+        List<String> words = Arrays.asList(
+            "Een",
+            "WorkShop",
+            "Voor",
+            "PAFR"
+        );
+        Observable<String> obs1 = Observable.from(words).map(s -> "(iets er voor?)" + s);
+        obs1.subscribe(new MyObserver());
+    }
+}
--- src/main/java/io/reactivex/reactive/PrintSubscriber.java
+++ src/main/java/io/reactivex/reactive/PrintSubscriber.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package reactive;
+package io.reactivex.reactive;
 
 import rx.Subscriber;
 
